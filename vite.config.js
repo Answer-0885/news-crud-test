b8ed3ac10b9f026f77dda1efc35bcc428b41f9ce
Vitaly <answer-0885@mail.ru>
2025-06-13 17:05:44 +0700

    news-crud-test

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/', // для корня домена
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  }
+  base: '/news-crud-test/', 
 })
